a6ae78e2883747dff9d6ba9423c8c0ffa8d24ce9
ghstbrd <jared.ddriver@gmail.com>
2025-01-07 19:31:36 -0600

    Update app.js

--- Starter_Code/static/js/app.js
+++ Starter_Code/static/js/app.js
@@ -1,7 +1,6 @@
 // Build the metadata panel
 function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
-<<<<<<< HEAD
     // Get the metadata field
     const metadata = data.metadata;
 
@@ -74,62 +73,12 @@ function buildCharts(sample) {
 
     // Render the Bar Chart
     Plotly.newPlot("bar", [barTrace], barLayout);
-=======
-
-    // get the metadata field
-
-
-    // Filter the metadata for the object with the desired sample number
-
-
-    // Use d3 to select the panel with id of `#sample-metadata`
-
-
-    // Use `.html("") to clear any existing metadata
-
-
-    // Inside a loop, you will need to use d3 to append new
-    // tags for each key-value in the filtered metadata.
-
-  });
-}
-
-// function to build both charts
-function buildCharts(sample) {
-  d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
-
-    // Get the samples field
-
-
-    // Filter the samples for the object with the desired sample number
-
-
-    // Get the otu_ids, otu_labels, and sample_values
-
-
-    // Build a Bubble Chart
-
-
-    // Render the Bubble Chart
-
-
-    // For the Bar Chart, map the otu_ids to a list of strings for your yticks
-
-
-    // Build a Bar Chart
-    // Don't forget to slice and reverse the input data appropriately
-
-
-    // Render the Bar Chart
-
->>>>>>> b2fba8a68ae9b2531da37fd49793c60af71a1a6b
   });
 }
 
 // Function to run on page load
 function init() {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
-<<<<<<< HEAD
     // Get the names field
     const names = data.names;
 
@@ -149,42 +98,15 @@ function init() {
     // Build charts and metadata panel with the first sample
     buildCharts(firstSample);
     buildMetadata(firstSample);
-=======
-
-    // Get the names field
-
-
-    // Use d3 to select the dropdown with id of `#selDataset`
-
-
-    // Use the list of sample names to populate the select options
-    // Hint: Inside a loop, you will need to use d3 to append a new
-    // option for each sample name.
-
-
-    // Get the first sample from the list
-
-
-    // Build charts and metadata panel with the first sample
-
->>>>>>> b2fba8a68ae9b2531da37fd49793c60af71a1a6b
   });
 }
 
 // Function for event listener
 function optionChanged(newSample) {
   // Build charts and metadata panel each time a new sample is selected
-<<<<<<< HEAD
   buildCharts(newSample);
   buildMetadata(newSample);
-=======
-
->>>>>>> b2fba8a68ae9b2531da37fd49793c60af71a1a6b
 }
 
 // Initialize the dashboard
 init();
-<<<<<<< HEAD
-
-=======
->>>>>>> b2fba8a68ae9b2531da37fd49793c60af71a1a6b
